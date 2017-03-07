c1fb8254c3dc622cc3c129bf5511d5861ead9356
Mame Awa Diop <mame-awa.diop@hec.ca>
2017-03-07 09:41:57 -0500

    ZCII-2818: Migrer et tester la job de création des événements Section-Aware ZCII-2819: Pour E2017: bloquer la nouvelle job aux sites du pilote

---
+++ api/src/java/ca/hec/jobs/api/HecCalendarEventsJob.java
@@ -0,0 +1,13 @@
+package ca.hec.jobs.api;
+
+import org.quartz.Job;
+
+/**
+ * Job Quartz pour créer les événements dans les calendriers sakai à partir de la table HEC_EVENT
+ * @author Curtis van Osch
+ *
+ */
+public interface HecCalendarEventsJob extends Job{
+
+
+}
