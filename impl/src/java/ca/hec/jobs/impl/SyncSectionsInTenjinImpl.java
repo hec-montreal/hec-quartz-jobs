f264e5fa55b1e7fd6611a29104b0bd0f5ec84857
Mame Awa Diop <mame-awa.diop@hec.ca>
2017-05-14 23:18:37 -0400

    ZCII-2935: Développer le version section-aware de la job synchro

--- impl/src/java/ca/hec/jobs/impl/SyncSectionsInTenjinImpl.java
+++ impl/src/java/ca/hec/jobs/impl/SyncSectionsInTenjinImpl.java
@@ -11,6 +11,7 @@ import org.apache.log4j.Logger;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.sakaiproject.coursemanagement.api.CourseManagementService;
+import org.sakaiproject.site.api.Group;
 import org.sakaiproject.site.api.Site;
 import org.sakaiproject.site.api.SiteService;
 import org.sakaiproject.tool.api.Session;
@@ -56,8 +57,10 @@ public class SyncSectionsInTenjinImpl implements SyncSectionsInTenjin {
 
         List<Site> allSites = siteService.getSites(SiteService.SelectionType.ANY, "course", null, null, SiteService.SortType.CREATED_ON_DESC, null);
         List<Syllabus> syllabi;
+        Syllabus common = null;
+        //Use groupIds
         Set<String> syllabusSections, newSyllabusSections, oldSyllabusSections;
-
+        Collection<Group> siteGroups = null;
 
         Session session = sessionManager.getCurrentSession();
 
@@ -65,60 +68,66 @@ public class SyncSectionsInTenjinImpl implements SyncSectionsInTenjin {
             session.setUserEid("admin");
             session.setUserId("admin");
 
-           do{
+            do {
                 site = allSites.get(counter++);
                 createdOn = site.getCreatedDate();
-               if (site.getId().equalsIgnoreCase("30-400-13.E2017")) {
-                if (createdOn.before(startingDate))
-                    continue;
-                if (site.getProviderGroupId() == null || site.getProviderGroupId().isEmpty())
-                    continue;
-                System.out.println(site.getProviderGroupId());
-                providerId = site.getProviderGroupId();
-               try {
+                if (site.getId().equalsIgnoreCase("30-400-13.E2017")) {
+                    if (createdOn.before(startingDate))
+                        continue;
+                    if (site.getProviderGroupId() == null || site.getProviderGroupId().isEmpty())
+                        continue;
+                    System.out.println(site.getProviderGroupId());
+                    providerId = site.getProviderGroupId();
+                    try {
 
-                       providerIds = new ArrayList<String>();
-                       Collections.addAll(providerIds, providerId.split("\\+"));
+                        providerIds = new ArrayList<String>();
+                        Collections.addAll(providerIds, providerId.split("\\+"));
 
-                       syllabi = syllabusService.getSyllabusList(site.getId());
+                        syllabi = syllabusService.getSyllabusList(site.getId());
 
-                       //Make sure all the sections in Tenjin are up to date
-                       //Delete cancelled sections
-                       for (Syllabus syllabus : syllabi) {
-                           syllabusSections = syllabus.getSections();
-                           newSyllabusSections = new HashSet<String>();
-                           oldSyllabusSections = new HashSet<String>();
-                           if (syllabusSections != null && !syllabusSections.isEmpty()) {
-                               for (String section : syllabusSections) {
-                                   if (providerIds.contains(section)) {
-                                       newSyllabusSections.add(section);
-                                       providerIds.remove(section);
-                                   }else{
-                                       oldSyllabusSections.add(section);
-                                   }
-                               }
-                               if (!providerIds.isEmpty()) {
-                                   newSyllabusSections.addAll(providerIds);
-                               }
-                              for(String newSectionId: newSyllabusSections) {
-                                   syllabusDao.addSection(syllabus.getId().toString(), newSectionId);
-                               }
+                        //Make sure all the sections in Tenjin are up to date
+                        //Delete cancelled sections
+                        for (Syllabus syllabus : syllabi) {
+                            if (syllabus.getCommon())
+                                common = syllabus;
+                            syllabusSections = syllabus.getSections();
+                            newSyllabusSections = new HashSet<String>();
+                            oldSyllabusSections = new HashSet<String>();
+                            if (syllabusSections != null) {
+                                for (String section : syllabusSections) {
+                                    if (providerIds.contains(section)) {
+                                        newSyllabusSections.add(section);
+                                        providerIds.remove(section);
+                                    } else {
+                                        oldSyllabusSections.add(section);
+                                    }
+                                }
+                                if (!providerIds.isEmpty()) {
+                                    newSyllabusSections.addAll(providerIds);
+                                }
+                                for (String oldSectionId : oldSyllabusSections) {
+                                    syllabusDao.deleteSection(syllabus.getId().toString(), oldSectionId);
+                                }
+                                System.out.println("To delete " + oldSyllabusSections.toString());
+                            }
 
-                               for(String oldSectionId: oldSyllabusSections){
-                                   syllabusDao.deleteSection(syllabus.getId().toString(), oldSectionId);
-                               }
-                               System.out.println("To delete " + oldSyllabusSections.toString());
-                           }
-                       }
+                            //Add new sections to common
+                            if (common != null)
+                                for (String newSectionId : newSyllabusSections) {
+                                    syllabusDao.addSection(common.getId().toString(), newSectionId);
+                                }
 
-                   } catch(NoSiteException e){
-                       //Will not happen but just in case
-                       log.error(e.getMessage());
-                   } catch(DeniedAccessException e){
-                       log.error("You are not allowed to access syllabi for the site " + site.getId() + " " + e.getMessage());
-                   }
-               }
-           } while (createdOn.after(startingDate));
+
+                        }
+
+                    } catch (NoSiteException e) {
+                        //Will not happen but just in case
+                        log.error(e.getMessage());
+                    } catch (DeniedAccessException e) {
+                        log.error("You are not allowed to access syllabi for the site " + site.getId() + " " + e.getMessage());
+                    }
+                }
+            } while (createdOn.after(startingDate));
         } finally {
             session.clear();
         }
