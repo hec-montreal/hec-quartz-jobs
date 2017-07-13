fb2379ead38e60fa255c50557643bedcd3d7c5db
Mame Awa Diop <mame-awa.diop@hec.ca>
2017-07-13 16:31:28 -0400

    ZCII-3015: Sortir la job du courseeventsynchro

---
+++ api/src/java/ca/hec/jobs/api/calendar/CourseEventSynchroJob.java
@@ -0,0 +1,16 @@
+package ca.hec.jobs.api.calendar;
+
+
+import org.quartz.Job;
+
+/**
+ * Job de synchro du fichier d'extract contenant les événements de cours avec la
+ * table HEC_EVENT
+ *
+ * @author 11183065
+ *
+ */
+public interface CourseEventSynchroJob extends Job {
+
+
+}
